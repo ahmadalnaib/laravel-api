280e17b33a6e5ca56a4405e3d136e0e609ee44b0
ahmedhabeeb73 <alnaib888@gmail.com>
2020-11-05 12:51:52 +0100

    add function for reisger and login

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -3,9 +3,51 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Dotenv\Validator;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Controllers\Api\BaseController as BaseController;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('please validate error', $validator->errors());
+        }
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('ahmed')->accessToken;
+        $success['name'] = $user->name;
+        return $this->sendResponse($success, 'user registerd successfully');
+    }
+
+
+    public function login(Request $request)
+    {
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('ahmed')->accessToken;
+            $success['name'] = $user->name;
+            return $this->sendResponse($success, 'user login successfully');
+
+        } else {
+            return $this->sendError('please check your auth', ['error'=>'Unauthorised']);
+        }
+
+
+    }
+
 }
