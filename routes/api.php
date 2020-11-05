ea3c7215c24aedf74770006142091e6cd3342cef
ahmedhabeeb73 <alnaib888@gmail.com>
2020-11-05 12:06:48 +0100

    set up api controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register','RegisterController@register');
+Route::post('register','RegisterController@login');
+
+
+Route::middleware('auth:api')->group(function (){
+//    Route::resource('posts',PostController);
+});
